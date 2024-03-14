c0c2485b30306d03145ff966c45b1342f785fa1d
Jakub Madry <jaiwiecejmnie@gmail.com>
2024-03-14 11:40:49 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/smarthome_koiot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smarthome_koiot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:smarthome_koiot/presentation/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
